27ea8dda21cd0a5ef779035467470922827bf2d3
kedary3 <kedary3@gmail.com>
2020-08-19 22:28:23 -0700

    ToE for rcp85 data

---
+++ src/Global_Vars.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 21:36:34 2020
+
+@author: Kedar Yadav
+"""
+#Global Variables
+mean=0
+std=0
--- src/Read_Plot.py
+++ src/Read_Plot.py
@@ -3,11 +3,22 @@
 """
 Created on Mon Jul  6 12:59:55 2020
 
-@author: esalathe
+@author: Kedar Yadav
 """
+from Global_Vars import mean as mn
+from Global_Vars import std 
 import numpy as np
 import matplotlib.pyplot as plt 
+from sklearn.metrics import r2_score
+
 # repeat for each file in list
+# from netCDF4 import Dataset
+# Net_data = Dataset("ccsm4_2006-2099_TX90th.nc", "r" ,format="NETCDF4")
+# print(Net_data)
+# for name in Net_data.ncattrs():
+#      print("Global attr {} = {}".format(name, getattr(Net_data, name)))
+                                                      
+# Net_data.close
 for file in [
         "ccsm4_tx90p_ksea_historicalNat.dat",
         "ccsm4_tx90p_ksea_rcp85.dat"
@@ -25,12 +36,44 @@ for file in [
     # plot the data
     plt.plot(year,tx90p)
     #to dos 
-    Standard_Deviation=np.std(tx90p)
-    print(Standard_Deviation)
-    # get the standard deviation of historical 
-    # alpha= sqrt((1/N)*sum (i=0 to N)(x(i)-mean)^2)
-    # linear fit the rcp predictied data
-    # calculate the ToE
-
+    
+    #if the current file is of historical data, find the standard deviation
+    if(file.__str__().find("historical")!=-1): 
+        
+        Standard_Deviation=np.std(tx90p)
+        #set globals so they can be accessed
+        std=Standard_Deviation 
+        mn=np.mean(tx90p)
+        #Plot the mean and standard deviation lines
+        x1= [year[0],int(year[len(year)-1]+len(year))]
+        y1= [mn+(Standard_Deviation),mn+(Standard_Deviation)] 
+        y2= [mn-(Standard_Deviation),mn-(Standard_Deviation)]
+        y3= [np.mean(tx90p),np.mean(tx90p)]
+        plt.plot(x1,y1,marker="o")
+        plt.plot(x1,y2,marker="o")
+        plt.plot(x1,y3,marker="o")
+    #if the current file is a model, find its linear regression and the ToE
+    if(file.__str__().find("historical")==-1): 
+        x=year
+        y=tx90p
+        Linear_Regression= np.polyfit(x,y,1)
+        predict=np.poly1d(Linear_Regression)
+        #slope of regression is lin_reg[0], y intercept is lin_reg[1]
+        #R2+score is the accuracy of the linear fit
+        r2_score=r2_score(y, predict(x))
+        x_lin_reg = range(int(year[0]), int(year[len(year)-1]))
+        y_lin_reg = predict(x_lin_reg)
+        #Plot the linear regression
+        plt.plot(x_lin_reg, y_lin_reg, c = 'black')
+        #Calculate the ToE
+        if(Linear_Regression[0]==0):
+            Time_of_Emergence="Never"
+        else:
+            Time_of_Emergence = (mn+(std)-Linear_Regression[1])/Linear_Regression[0]
+            print("The Time of Emergence is " + Time_of_Emergence.__str__())
+            t1,y1 = [Time_of_Emergence,Time_of_Emergence], [0,np.std(tx90p)]
+            #Plot the ToE line
+            plt.plot(t1,y1, marker="_")
+   
     
     
