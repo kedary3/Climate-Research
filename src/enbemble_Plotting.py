2705d49c5a34064edd8d0630ea7ca783a08988e4
kedary3 <kedary3@gmail.com>
2021-05-06 03:07:12 -0700

    ensemble plotting is completed for TOE,Slopes, and SDV

--- src/enbemble_Plotting.py
+++ src/enbemble_Plotting.py
@@ -29,6 +29,92 @@ from PIL import Image
 import matplotlib.image as mpimg
 import sys
 
+def Generate_Three_Pane_Slope_Plots():
+    models = ["access1.3","bcc","canesm2","ccsm4","csiro","fgoals","gfdl","giss","miroc5","mri","noresm1"]
+    WRF_Slope_Folder = r"WRF Slope Plots" 
+    GCM_Slope_Folder = r"GCM Slope Plots"
+    Delta_Slope_Folder = r"Slope Deltas"
+    
+    t_Data_Types = ["tasmax90"]
+    p_Data_Types = ["pr95"]
+    
+    for model in models:
+        for temperature_Type in t_Data_Types:
+             Images = []
+             wrf_Data_Type = " "
+             if(temperature_Type == "tasmax90"):
+                 wrf_Data_Type = "T2MAX90"
+             elif(temperature_Type == "tasmaxx"):
+                 wrf_Data_Type = "T2MAXx"
+             elif(temperature_Type !="tasmax90" and temperature_Type != "tasmaxx"):
+                 print("invalid temperature_Type")
+                 return 1
+             for WRF_IMG in os.listdir(WRF_Slope_Folder):
+                 WRF_IMG_HEAD , WRF_IMG_TAIL = os.path.split(WRF_IMG)
+                 if(WRF_IMG_TAIL.__str__().find(model)>=0 and WRF_IMG_TAIL.__str__().find(wrf_Data_Type)>=0):
+                     Images.append(Image.open(WRF_Slope_Folder + "\\"+ WRF_IMG))
+             for GCM_IMG in os.listdir(GCM_Slope_Folder): 
+                 GCM_IMG_HEAD , GCM_IMG_TAIL = os.path.split(GCM_IMG)
+                 if(GCM_IMG_TAIL.__str__().find(model)>=0 and GCM_IMG_TAIL.__str__().find(temperature_Type)>=0):
+                     Images.append(Image.open(GCM_Slope_Folder + "\\"+GCM_IMG))
+             for Delta_IMG in os.listdir(Delta_Slope_Folder): 
+                 Delta_IMG_HEAD , Delta_IMG_TAIL = os.path.split(Delta_IMG)
+                 if(Delta_IMG_TAIL.__str__().find(model)>=0 and Delta_IMG_TAIL.__str__().find(temperature_Type)>=0):
+                     Images.append(Image.open(Delta_Slope_Folder + "\\"+Delta_IMG))
+        
+     
+             widths, heights = zip(*(i.size for i in Images))
+            
+             total_width = sum(widths)
+             max_height = max(heights)
+            
+             new_im = Image.new('RGBA', (total_width, max_height))
+            
+             x_offset = 0
+             for im in Images:
+                 new_im.paste(im, (x_offset,0))
+                 x_offset += im.size[0]
+
+             new_im.save("Ensemble Slope Plots"+ "\\" + "Ensemble Slope Plot with " + model + " based on " + temperature_Type+".png")
+        for precipitation_Type in p_Data_Types:
+             Images = []
+             wrf_Data_Type = " "
+             if(precipitation_Type == "pr95"):
+                 wrf_Data_Type = "PREC95"
+             elif(precipitation_Type == "prx"):
+                 wrf_Data_Type = "PRECx"
+             elif(precipitation_Type != "prx" and precipitation_Type != "pr95"):
+                 print("invalid precipitation_Type")
+                 return 1
+             for WRF_IMG in os.listdir(WRF_Slope_Folder):
+                 WRF_IMG_HEAD , WRF_IMG_TAIL = os.path.split(WRF_IMG)
+                 if(WRF_IMG_TAIL.__str__().find(model)>=0 and WRF_IMG_TAIL.__str__().find(wrf_Data_Type)>=0):
+                     Images.append(Image.open(WRF_Slope_Folder + "\\"+ WRF_IMG))
+             for GCM_IMG in os.listdir(GCM_Slope_Folder): 
+                 GCM_IMG_HEAD , GCM_IMG_TAIL = os.path.split(GCM_IMG)
+                 if(GCM_IMG_TAIL.__str__().find(model)>=0 and GCM_IMG_TAIL.__str__().find(precipitation_Type)>=0):
+                     Images.append(Image.open(GCM_Slope_Folder + "\\"+GCM_IMG))
+             for Delta_IMG in os.listdir(Delta_Slope_Folder): 
+                 Delta_IMG_HEAD , Delta_IMG_TAIL = os.path.split(Delta_IMG)
+                 if(Delta_IMG_TAIL.__str__().find(model)>=0 and Delta_IMG_TAIL.__str__().find(precipitation_Type)>=0):
+                     Images.append(Image.open(Delta_Slope_Folder + "\\"+Delta_IMG))
+        
+     
+             widths, heights = zip(*(i.size for i in Images))
+            
+             total_width = sum(widths)
+             max_height = max(heights)
+            
+             new_im = Image.new('RGBA', (total_width, max_height))
+            
+             x_offset = 0
+             for im in Images:
+                 new_im.paste(im, (x_offset,0))
+                 x_offset += im.size[0]
+
+             new_im.save("Ensemble Slope Plots"+ "\\" + "Ensemble Slope Plot with "+ model + " based on " + precipitation_Type+".png")
+
+
 def Generate_Three_Pane_SDV_Plots():
     models = ["access1.3","bcc","canesm2","ccsm4","csiro","fgoals","gfdl","giss","miroc5","mri","noresm1"]
     WRF_SDV_Folder = r"WRF SDV Plots" 
@@ -75,7 +161,7 @@ def Generate_Three_Pane_SDV_Plots():
                  new_im.paste(im, (x_offset,0))
                  x_offset += im.size[0]
 
-             new_im.save("Ensemble Plots"+ "\\" + "Ensemble SDV Plot with " + model + " based on " + temperature_Type+".png")
+             new_im.save("Ensemble SDV Plots"+ "\\" + "Ensemble SDV Plot with " + model + " based on " + temperature_Type+".png")
         for precipitation_Type in p_Data_Types:
              Images = []
              wrf_Data_Type = " "
@@ -162,7 +248,7 @@ def Generate_Three_Pane_TOE_Plots():
                  new_im.paste(im, (x_offset,0))
                  x_offset += im.size[0]
 
-             new_im.save("Ensemble Plots"+ "\\" + "Ensemble TOE Plot with " + model + " based on " + temperature_Type+".png")
+             new_im.save("Ensemble TOE Plots"+ "\\" + "Ensemble TOE Plot with " + model + " based on " + temperature_Type+".png")
         for precipitation_Type in p_Data_Types:
              Images = []
              wrf_Data_Type = " "
@@ -204,3 +290,5 @@ def Generate_Three_Pane_TOE_Plots():
         
 #execute
 Generate_Three_Pane_TOE_Plots()
+Generate_Three_Pane_SDV_Plots()
+Generate_Three_Pane_Slope_Plots()
---
+++ src/ensemble_SDV_Plotting.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 02:43:19 2021
+
+@author: Kedar
+"""
+
